a6a2834520b0fe8b0eff5eba5ee8ec7c66e9fa5e
jahidur17 <jahidurrahman9117@gmail.com>
2025-08-15 02:42:55 +0600

    added the problem no.4 and this problem solve

--- main.js
+++ main.js
@@ -33,3 +33,20 @@ function bestTeam(player1, player2) {
   }
 }
 
+
+function isSame(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid";
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let n = 0; n < arr1.length; n++) {
+    if (arr1[n] === arr2[n]) {
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
--- practice-4.js
+++ practice-4.js
@@ -6,12 +6,12 @@ function isSame(arr1, arr2) {
     return false;
   }
   for (let n = 0; n < arr1.length; n++) {
-    if (arr1[n] === arr2[n]) {
-    } else {
-      return false;
+    if (arr1[n] === arr2[n]){
+        return false;
     }
+        
   }
   return true;
 }
-const result = isSame([1, 4, 5], [1, 4, 5]);
+const result = isSame([1, 4, 6], [1, 4, 5]);
 console.log(result);
